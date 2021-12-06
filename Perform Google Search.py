fb4e57b3909a83c51b85ced3e455b6a38b1af2a9
SwaroopBolisetty <87690969+SwaroopBolisetty@users.noreply.github.com>
2021-12-06 19:45:28 +0530

    Add files via upload

---
+++ Perform Google Search.py
@@ -0,0 +1,33 @@
+'''
+Python Program to Perform Google Search
+'''
+
+# Import the necessary module!
+import pywhatkit as kt
+
+###############################################
+# Method 1:
+
+# Display welcome message
+print("Let's perform Google search!")
+
+# Single search item
+search1 = "SRM UNIVERSITY"
+
+# Options for multiple search items
+search2 = "github"
+search3 = "YOUTUBE"
+search4 = "LINKEDIN"
+
+# Call search the method
+kt.search(search4)
+
+###############################################
+# Method 2:
+
+# Import pywhatkit as kt
+# kt.search("python4DataScience")
+
+
+# NOTE: If your browser window is open is already running, 
+# it will perform the search in a new tab otherwise will open a new window.
